2f16540d884092d1b9cd4fbf91f7fdf45c21a0dc
Carlos Eduardo Oliveira (Kadu) - Turma 24 - Tribo A <carloseduardooliveira81@gmail.com>
2023-09-06 16:15:28 -0300

    criação da aplicaçao blog

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect} from 'react';
+import axios from 'axios';
+import Post from './Post';
 
-function App() {
+
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => {
+        setPosts(res.data);
+      }
+    )
+    .catch(err => {
+      console.log(err);
+    });
+  }, []);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const handleSearch = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredPosts = posts.filter((post) => {
+    return post.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      post.body.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Blog Posts</h1>
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={handleSearch}
+        />
+      <div className="pagination">
+        <button onClick={() => handlePageChange(currentPage - 1)}>
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button onClick={() => setCurrentPage(currentPage + 1)}>
+          Next
+        </button>
+      </div>
+      {filteredPosts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
+
+
